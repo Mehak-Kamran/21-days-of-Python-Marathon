d4559b4d473566d49812ae7f8a2b6de20f2153b1
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-03-20 19:23:55 +0000

    deleting

--- Quiz Game/quizgame.py
+++
@@ -1,9 +0,0 @@
-#Dataware House and Data Mining Quiz Game
-
-print("======Hello There, This is DWM Quiz Game======")
-ready=input("Are you ready to play the Quiz ?").lower()
-
-if(ready=="yes")
-  print("Great")
-else:
-    quit()
