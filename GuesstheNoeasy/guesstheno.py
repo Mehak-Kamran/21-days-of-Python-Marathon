dd4bae3245bb90969dd63ada6fac3930cfc85e3b
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-03-24 18:45:22 +0000

    guess the no

--- GuesstheNoeasy/guesstheno.py
+++ GuesstheNoeasy/guesstheno.py
@@ -2,26 +2,44 @@
 
 import random
 
-print(".........Welcome........ ")
 
 
-compguess=random.randint(1,40)
-print(compguess)
-print(" Guess the Number between 1-40 \n")
-
-while True:
-  userguess=int(input())
-  if userguess.isdigit():
-     if compguess==userguess:
-                print("Congratulations You got it right....")
+def playgame():
+    compguess=random.randint(1,40)
+    print(compguess)
+    print(" Guess the Number between 1-40 \n")
+    guess=0
+    while True:
+        userguess = input()
+        guess += 1
+        if userguess.isdigit():
+            userguess = int(userguess)
+            if compguess == userguess:
+                print("Congratulations You got it right in", guess, "guesses....")
                 break
-     elif userguess>compguess:
+            elif userguess > compguess:
                 print("Try to guess a lower no")
-     else:
+            else:
                 print("Try to guess a higher no")
-  else:
-    print("Please Enter a no Next time...")
-    break 
+        else:
+            print("Please Enter a no Next time...")
+            break 
+
+
+
+print(".........Welcome........ ")
+choice=input("\n Press 'q' to quit:\n Press 'c' to continue:\n")
+
+
+
+
+if choice=="q":
+  quit
+else:
+  
+  playgame()
+
+
 
 
 
