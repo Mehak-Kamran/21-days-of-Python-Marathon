873d394ab70805155be9cd6fee78b1e346f91ac7
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-03-24 18:03:20 +0000

    checking if userinput is digit

--- GuesstheNoeasy/guesstheno.py
+++ GuesstheNoeasy/guesstheno.py
@@ -6,16 +6,22 @@ print(".........Welcome........ ")
 
 
 compguess=random.randint(1,40)
+print(compguess)
+print(" Guess the Number between 1-40 \n")
 
 while True:
-  userguess=int(input(" Guess the Number between 1-40 \n"))
-  if compguess==userguess:
-    print("Congratulations You got it right....")
-    break
-  elif userguess>compguess:
-    print("Try to guess a lower no")
+  userguess=int(input())
+  if userguess.isdigit():
+     if compguess==userguess:
+                print("Congratulations You got it right....")
+                break
+     elif userguess>compguess:
+                print("Try to guess a lower no")
+     else:
+                print("Try to guess a higher no")
   else:
-   print("Try to guess a higher no")
+    print("Please Enter a no Next time...")
+    break 
 
 
 
