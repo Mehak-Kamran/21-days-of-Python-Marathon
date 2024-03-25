741ec1fdb8f37fb7be9008a2598e069031585a94
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-03-25 18:29:51 +0000

    scoring

--- rockpaperscissoreasy/rps.py
+++ rockpaperscissoreasy/rps.py
@@ -1,7 +1,13 @@
 #Rock Paper Scissor Easy
 import random
 
-def playgame():
+scorecomp=0
+scoreuser=0
+
+print("\nWelcome\n")
+
+
+def playgame(scorecomp,scoreuser):
     options=['rock','paper','scissor']
     userchoice=input("Enter your choice: Rock, Paper or Scissor\n").lower()
     if userchoice not in options:
@@ -10,21 +16,23 @@ def playgame():
     compchoice= random.choice(options)
     print(compchoice)
     if userchoice=='scissor' and compchoice=='paper' or userchoice=='paper' and compchoice=='rock' or userchoice=='rock' and compchoice=='scissor' :
-        print("Congratulations You win")
+        print("Congratulations You win\n")
+        scoreuser+=1
     elif userchoice==compchoice:
-        print("Game is Drawn")
+        print("Game is Drawn\n")
     else:
-        print("You loose")
-
+        print("You loose\n")
+        scorecomp+=1
+    return scorecomp,scoreuser
 
 while True:
-    choice=input("Welcome\n Press 'c' to continue\n Press 'q' to quit\n").lower()
+    choice=input("Press 'c' to continue\nPress 'q' to quit\n").lower()
 
     if choice=='q':
-        quit()
+        break
     elif choice=='c':
-        playgame()
+        scorecomp,scoreuser=playgame(scorecomp,scoreuser)
     else:
         print("Press 'c' to continue and Press 'q' to quit next time ")
 
-print("===Game Over===")
+print("Your score",scoreuser,"Computer score",scorecomp,"\n===Game Over===")
