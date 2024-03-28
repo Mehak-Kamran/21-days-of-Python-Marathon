04d59d7654f57c496e8020ec766545a178930dc0
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-03-28 17:51:18 +0000

    encryption using Fernet

--- passwordManager/passman.py
+++ passwordManager/passman.py
@@ -1,35 +1,41 @@
 # Password Manager + Encryption
 #pip install cryptography
 
-from cryptography.fernet import  Fernet
+from cryptography.fernet import Fernet
 
-masterpass=input("Enter your Master password:  ")
-
-def keydef():
+'''def keydef():
    key = Fernet.generate_key()
-   with open("key.key","wb") as f:#write in bytes
-      f.write(key)
+   with open("passwordManager/key.key","wb") as f:#write in bytes
+      f.write(key)'''
+
+#keydef()
+
+
+def loadkey():
+  file= open("passwordManager/key.key","rb")
+  key=file.read()
+  file.close()
+  return key
+
+
+
+key=loadkey()
+fer=Fernet(key)
+
 
 
 def add():
     name=input("Enter your name: ")
     paswd=input("Enter your password: ")
     with open("passwordManager/password.txt","a") as f:
-        f.write(name +"|"+ paswd+"\n")
+        f.write(name +"|"+ fer.encrypt(paswd.encode()).decode()+"\n")
 
 def view():
    with open("passwordManager/password.txt","r") as f:
       for line in f.readlines():
          data=line.rstrip()
-         name,paswd=data.split("|")
-         print("name:",name,"password:",paswd)
-
-         
-         
-
-       
-
-
+         name,paa=data.split("|")
+         print("name:",name,"password:", fer.decrypt(paa.encode()).decode())
 
 
 
