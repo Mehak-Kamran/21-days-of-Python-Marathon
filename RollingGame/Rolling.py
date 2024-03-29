3f7e574f9e9342e0faa92b7c1fe33e0c786d8065
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-03-29 14:31:50 +0000

    initials

--- RollingGame/Rolling.py
+++ RollingGame/Rolling.py
@@ -1,10 +1,24 @@
+import random
+
+def roll():
+    min=1
+    max=6
+    value=random.randint(min,max)
+    return value
+
+
 while True:
     players=input("Enter No of players(2-4): ")
     if players.isdigit():
         players=int(players)
         if 2<=players<=4:
-            #roll()
-            break
+            if val != 1:
+                val=roll()
+                print(val)
+            else:
+                break
+
+            
         else:
             print("Enter valid range (2-4): ")
             
