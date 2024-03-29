6f851bd0f75ebc10bee966498445b6453b0bd97a
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-03-29 14:12:09 +0000

    initials

--- RollingGame/Rolling.py
+++ RollingGame/Rolling.py
@@ -1,1 +1,13 @@
-players=input("Enter No of players(2-4): ")
+while True:
+    players=input("Enter No of players(2-4): ")
+    if players.isdigit():
+        players=int(players)
+        if 2<=players<=4:
+            #roll()
+            break
+        else:
+            print("Enter valid range (2-4): ")
+            
+    else:
+        print("Enter a Integer")
+        
