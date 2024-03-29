e0e1eab38c845ca09e6bfec8c1951d65b4547e73
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-03-29 16:02:05 +0000

    setting up turns

--- RollingGame/Rolling.py
+++ RollingGame/Rolling.py
@@ -6,17 +6,35 @@ def roll():
     value=random.randint(min,max)
     return value
 
+def score(val):
+    
+            if val!= 1:
+                while True:
+                    y=input("do you want to play(y):")
+                    if y=="y":
+                        rolling()
+                    else:
+                         quit()        
+        
+            else:
+                quit()
+       
+
+def rolling():
+    val=roll()
+    print(val) 
+    
+    score(val) 
+        
+
 
 while True:
     players=input("Enter No of players(2-4): ")
     if players.isdigit():
         players=int(players)
         if 2<=players<=4:
-            if val != 1:
-                val=roll()
-                print(val)
-            else:
-                break
+            rolling()
+            
 
             
         else:
@@ -25,3 +43,5 @@ while True:
     else:
         print("Enter a Integer")
         
+
+
