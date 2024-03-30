6adad9e2e8a2ddadab38c0fc664dc35c3a791081
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-03-30 11:17:41 +0000

    initials

---
+++ storyTell/matlibgame.py
@@ -0,0 +1,32 @@
+# read story from a file 
+with open("storyTell/story.txt","r") as f:
+    story=f.read()
+#print(story)
+
+startindex=-1
+targetstart="<"
+targetend=">"
+keys=set()
+
+for index, char in enumerate(story):
+    if char==targetstart:
+        startindex=index
+    if char==targetend and startindex!=-1:
+        key=story[startindex:index+1]
+        keys.add(key)
+        startindex=-1
+
+#print(keys)
+
+dic={}
+
+for key in keys:
+    keyvalue=input("Enter answer for"+ key +": ")
+    dic[key]=keyvalue
+
+#print(dic)
+for key in keys:
+    story=story.replace(key,dic[key])
+
+print(story)
+
