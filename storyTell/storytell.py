8c1e284d9653064ea93a2a21f5602e8f370c8559
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-03-30 10:50:51 +0000

    game

---
+++ storyTell/storytell.py
@@ -0,0 +1,37 @@
+# story Telling
+
+#read the story.txt file 
+with open("storyTell/story.txt","r")as f:
+    story=f.read()
+#print(story)
+
+#bc i dont need any duplicatinos
+words=set()
+targetstart="<"
+targetend=">"
+start=-1
+# enumerator loop because i need index as well as char
+for i, char in enumerate(story):
+    if char==targetstart:
+        start=i
+    if char==targetend and start!=-1:
+        word=story[start:i+1]
+        words.add(word)
+        #reset
+        start=-1
+#print(words)
+
+#now setting up dictionary
+answers={}
+#adding input in dictionary
+for word in words:
+    answer=input("Enter a word for"+ word +": ")
+    #inputting in dictionary 
+    answers[word]=answer
+    
+
+#print(answers)
+for word in words:
+    story=story.replace(word,answers[word])
+    
+print(story)
