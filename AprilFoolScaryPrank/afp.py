8ebe0fe9faa8ac34a502724b1f484c8b9013a930
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-04-02 18:09:17 +0000

    April Fools scary Prank using pygame

--- AprilFoolScaryPrank/afp.py
+++ AprilFoolScaryPrank/afp.py
@@ -8,15 +8,15 @@ pygame.init()
 screen=pygame.display.set_mode((0,0),pygame.FULLSCREEN)#since i want to use whole screen
 #playing and loading sound
 pygame.mixer.init()
-pygame.mixer.music.load("Halloween/ratsasan.mp3")
+pygame.mixer.music.load("AprilFoolScaryPrank/ratsasan.mp3")
 pygame.mixer.music.play()
 sleep(3)
 #playing again
-pygame.mixer.music.load("Halloween/scary.mp3")
+pygame.mixer.music.load("AprilFoolScaryPrank/scary.mp3")
 pygame.mixer.music.play()
 sleep(2)
 #loading img
-photo=pygame.image.load("Halloween/scr.jpg")
+photo=pygame.image.load("AprilFoolScaryPrank/scr.jpg")
 screen.blit(photo,(0,0))
 pygame.display.update()
 sleep(3)
